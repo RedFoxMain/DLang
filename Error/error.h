4e41528b462052031fc0a7e4238d63b5a0597196
fox102447@gmail.com <fox102447@gmail.com>
2025-10-26 14:20:13 +0300

    Made it better

--- Error/error.h
+++ Error/error.h
@@ -3,37 +3,9 @@
 
 #include <iostream>
 #include <exception>
+#include <format>
 
-#include "../Parser/parser.h"
-#include "../Parser/Lexer/lexer.h"
-
-// Class for Lexical errors
-class LexicalError: public std::exception {
-private:
-	std::string message_;
-
-public:
-	LexicalError(Lexer& lexer) {
-		this->message_ = "LexicalError(" + lexer.file_name + "): " + lexer.stream.code + "\nUnknown symbol (" 
-			+ lexer.stream.current_char
-			+ ") in " + std::to_string(lexer.stream.line) + ":" + std::to_string(lexer.stream.column);
-	}
-	const char* what() const throw() {
-		return this->message_.c_str();
-	}
-};
-
-// Class for Syntax errors
-class SyntaxError : public std::exception {
-private:
-	std::string message_;
-
-public:
-	SyntaxError(Parser& parser) {
-		this->message_ = "Synatx error occured\n";
-	}
-	const char* what() const throw() {
-		return this->message_.c_str();
-	}
-};
+static void raiseError(const std::string& msg) {
+	throw std::runtime_error(msg);
+}
 #endif // !ERROR_H
